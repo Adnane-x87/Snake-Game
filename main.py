1be0e507c8e906b7da541b4334e9bf5dc770b022
Adnane Kes <tangerinoh40@gmail.com>
2025-10-03 13:01:40 +0100

    new commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import Tk, Canvas, Label
+import random
+
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
@@ -13,6 +15,12 @@ class Snake:
     pass
 
 class Food:
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH//SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT//SPACE_SIZE) - 1) * SPACE_SIZE
+        self.coordinates = [x, y]
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
     def next_turn(self):
         pass
 
@@ -38,4 +46,20 @@ label.pack()
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height= window.winfo_screenheight()
+
+x = int((screen_width/2)  - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+ 
+Snake = Snake()
+food = Food()
+ 
 window.mainloop()
